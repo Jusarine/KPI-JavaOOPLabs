1b9541dd42f12a71985972f19ef6ff3731bc3fe4
Jusarine <tgladka6@gmail.com>
2018-03-16 17:56:31 +0200

    Initial commit

--- src/Lab3/Lab3.java
+++ src/Lab3/Lab3.java
@@ -19,49 +19,7 @@ public class Lab3 {
 
         readFile();
         findPalindromes(text.toString().split("\\W+"));
-        findMaxPalindrome(res.toString().split("\n"));
-
-    }
-    private static void findPalindromes(String[] arr){
-        res = new StringBuffer();
-
-        System.out.println("Substrings: " + Arrays.toString(arr));
-
-        for (String elem : arr) {
-            elem = elem.toLowerCase();
-
-            for (int i = 1; i < elem.length()-1; i++) {
-                for (int j = 1; (i + j) < elem.length(); j++) {
-                    if (j < (i + 1) && elem.charAt(i - j) == elem.charAt(i + j)) { // type ana
-                        res.append(elem.substring(i - j, i + j + 1));
-                        res.append("\n");
-                    }
-                    else break;
-                }
-                for (int j = 1; (i + j) < elem.length(); j++) {
-                    if (j < (i + 2) && elem.charAt(i - j + 1) == elem.charAt(i + j)) { // type anna
-                        res.append(elem.substring(i - j + 1, i + j + 1));
-                        res.append("\n");
-                    }
-                    else break;
-                }
-            }
-
-        }
-
-    }
-
-    private static void findMaxPalindrome(String[] arr){
-        System.out.println("Palindromes: " + Arrays.toString(arr));
-
-        String max = arr[0];
-        for (String aList : arr) {
-            if (aList.length() > max.length()) {
-                max = aList;
-            }
-        }
-
-        System.out.println("Max palindrome: " + max);
+        findMaxPalindrome(res.toString().split("\r"));
 
     }
 
@@ -80,8 +38,49 @@ public class Lab3 {
         if (sc != null) {
             while (sc.hasNext()) {
                 text.append(sc.nextLine());
-                text.append("\n");
+                text.append("\r");
             }
         }
+    }
+
+    private static void findPalindromes(String[] arr){
+        res = new StringBuffer();
+
+        System.out.println("Substrings: " + Arrays.toString(arr));
+
+        for (String elem : arr) {
+            elem = elem.toLowerCase();
+
+            for (int i = 1; i < elem.length()-1; i++) {
+                for (int j = 1; (i + j) < elem.length(); j++) {
+                    if (j < (i + 1) && elem.charAt(i - j) == elem.charAt(i + j)) { // type ana
+                        res.append(elem.substring(i - j, i + j + 1));
+                        res.append("\r");
+                    }
+                    else break;
+                }
+                for (int j = 1; (i + j) < elem.length(); j++) {
+                    if (j < (i + 2) && elem.charAt(i - j + 1) == elem.charAt(i + j)) { // type anna
+                        res.append(elem.substring(i - j + 1, i + j + 1));
+                        res.append("\r");
+                    }
+                    else break;
+                }
+            }
+        }
+    }
+
+    private static void findMaxPalindrome(String[] arr){
+        System.out.println("Palindromes: " + Arrays.toString(arr));
+
+        String max = arr[0];
+        for (String elem : arr) {
+            if (elem.length() > max.length()) {
+                max = elem;
+            }
+        }
+
+        System.out.println("Max palindrome: " + max);
+
     }
 }
